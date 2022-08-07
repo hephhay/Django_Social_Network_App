a52c8a2ef05eb5823b11dd8ebe3b68c934e87b96
hephhay <faradaydanfard@gmail.com>
2022-08-07 18:07:01 +0100

    set up chatsocket

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,16 +1,44 @@
 """
-ASGI config for myproject project.
+ASGI config for backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import dotenv
+import pathlib
 
+CURRENT_DIR = pathlib.Path(__file__).resolve().parent
+BASE_DIR = CURRENT_DIR.parent
+ENV_FILE_PATH = BASE_DIR / ".env"
+
+if ENV_FILE_PATH.exists():
+    dotenv.read_dotenv(str(ENV_FILE_PATH))
+
+os.environ['DJANGO_SETTINGS_MODULE'] =  'myproject.settings'
+
+import django
+django.setup()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+from chat.routing import websocket_urlpatterns
 
-application = get_asgi_application()
+django_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        )
+    )
+})
