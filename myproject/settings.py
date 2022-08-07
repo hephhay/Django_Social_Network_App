7c19e5a643549f91a82faf7fb7fd198a2fc79797
hephhay <faradaydanfard@gmail.com>
2022-08-07 15:25:10 +0100

    configure github oauth

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,7 +32,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
+
+SITE_ID = 1
 
 
 # Application definition
