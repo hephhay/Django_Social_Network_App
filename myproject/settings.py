29c39ddb829bfb09a8f4a6350503c4126f0e05b9
hephhay <faradaydanfard@gmail.com>
2022-08-07 12:46:47 +0100

    configure whitenoise

--- myproject/settings.py
+++ myproject/settings.py
@@ -78,7 +78,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'myproject.urls'
 
