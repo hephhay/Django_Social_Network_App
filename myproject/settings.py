a653ec6f8d7fed06e6e6e86d6211e3617c586795
hephhay <faradaydanfard@gmail.com>
2022-08-07 15:32:02 +0100

    configure github oauth

--- myproject/settings.py
+++ myproject/settings.py
@@ -34,8 +34,6 @@ DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
-SITE_ID = 1
-
 
 # Application definition
 
@@ -212,7 +210,7 @@ CHANNEL_LAYERS = {
     },
 }
 
-SITE_ID = 2     # considering 2nd site in 'Sites' to be 127.0.0.1 (for dev)
+SITE_ID = 1     # considering 2nd site in 'Sites' to be 127.0.0.1 (for dev)
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
