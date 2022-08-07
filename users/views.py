44e516e2c909498f24fabc76d9d1ecd9317143a2
hephhay <faradaydanfard@gmail.com>
2022-08-07 16:49:26 +0100

    removed captcha and google oauth

--- users/views.py
+++ users/views.py
@@ -55,21 +55,21 @@ def register(request):
         if form.is_valid():
 
             # reCAPTCHA V2
-            recaptcha_response = request.POST.get('g-recaptcha-response')
-            data = {
-                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
-                'response': recaptcha_response
-            }
-            r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
-            result = r.json()
+            # recaptcha_response = request.POST.get('g-recaptcha-response')
+            # data = {
+            #     'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+            #     'response': recaptcha_response
+            # }
+            # r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+            # result = r.json()
 
-            if result['success']:
+            # if result['success']:
                 form.save()
                 username = form.cleaned_data.get('username')
                 messages.success(request, f"Your account has been created! You can login now")
                 return redirect('login')
-            else:
-                messages.error(request, 'Invalid reCAPTCHA. Please try again.')            
+            # else:
+            #     messages.error(request, 'Invalid reCAPTCHA. Please try again.')            
             
     else:
         form = UserRegisterForm()
