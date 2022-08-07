860815f2f2be998b28375747f3cbdf4a1941f5f9
hephhay <faradaydanfard@gmail.com>
2022-08-07 13:01:31 +0100

    configure host

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,9 +32,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = [
-    'word-bucket.herokuapp.com'
-] 
+ALLOWED_HOSTS = ['*'] 
 
 
 # Application definition
